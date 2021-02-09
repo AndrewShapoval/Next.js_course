baeb73c281e29a14461cfb4f858c5689868e381e
AndrewShapoval <andrewshapoval88@gmail.com>
2021-02-09 15:56:55 +0300

    first commit

---
+++ pages/about/index.tsx
@@ -0,0 +1,30 @@
+import Router from "next/router";
+import {MainLayout} from "../../components/MainLayout";
+
+interface AboutPageProps {
+    title: string
+}
+
+export default function About({title}: AboutPageProps) {
+
+    const linkClickHandler = () => {
+        Router.push('/')
+    }
+
+    return (
+        <MainLayout title={"About page"}>
+            <h1>{title}</h1>
+            <button onClick={linkClickHandler}>Go back to home</button>
+            <button onClick={() => Router.push('/posts')}>Go to posts</button>
+        </MainLayout>
+    )
+}
+
+About.getInitialProps = async () => {
+    const response = await fetch(`${process.env.API_URL}/about`)
+    const data: AboutPageProps = await response.json()
+
+    return {
+        title: data.title
+    }
+}
---
+++ pages/post/[id].tsx
@@ -0,0 +1,75 @@
+import {useRouter} from "next/router";
+import {MainLayout} from "../../components/MainLayout";
+import Link from "next/link";
+import {useState, useEffect} from "react"
+import {NextPageContext} from "next";
+import {MyPost} from "../../interfaces/post";
+
+interface PostPageProps {
+    post: MyPost
+}
+
+export default function Post({post: serverPost}: PostPageProps) {
+    const router = useRouter()
+    const [post, setPost] = useState(serverPost)
+
+    useEffect(() => {
+        async function load() {
+            const response = await fetch(`${process.env.API_URL}/posts/${router.query.id}`)
+            const data = await response.json()
+            setPost(data)
+        }
+
+        if (!serverPost) {
+            load()
+        }
+    }, [])
+
+    if (!post) {
+        return (
+            <MainLayout>
+                <p>Loading...</p>
+            </MainLayout>
+        )
+    }
+
+    return (
+        <MainLayout>
+            {/*<h1>Post {router.query.id}</h1>*/}
+            <h1>{post.title}</h1>
+            <hr/>
+            <p>{post.body}</p>
+            <Link href="/posts"><a>Back to all posts</a></Link>
+        </MainLayout>
+    )
+}
+
+interface PostNextPageContext extends NextPageContext{
+    query: {
+        id: string
+    }
+}
+
+Post.getInitialProps = async (ctx: PostNextPageContext) => {
+
+    if (!ctx.req) {
+        return {post: null}
+    }
+
+    const response = await fetch(`http://localhost:4200/posts/${ctx.query.id}`)
+    const post: MyPost = await response.json()
+
+    return {
+        post
+    }
+}
+
+// export async function getServerSideProps(ctx) {
+//     // if (!ctx.req) {
+//     //     return {post: null}
+//     // }
+//     const response = await fetch(`http://localhost:4200/posts/${ctx.query.id}`)
+//     const post = await response.json()
+//
+//     return {props: {post}}
+// }
